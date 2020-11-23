7fb5eead10b8a46bb3b400c29f485558791224cb
PRAMODKPK123 <51121014+PRAMODKPK123@users.noreply.github.com>
2020-11-23 15:17:48 +0530

    Add files via upload

---
+++ Module03/iteratorDemo.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class IteratorDemo extends LightningElement {
+
+  contacts = [
+    {
+        Id: 'No.1',Name: 'Pramod Kumar',Email: 'pramodkumar.pk423@gmail.com',
+    },
+    {
+        Id: 'No.2',Name: 'Sunil Ukey',Email: 'sunil.ukey@gmail.com',
+    },
+    {
+        Id: 'No.3',Name: 'praful gadge',Email: 'prafulgadge@gmail.com',
+    },
+    {
+        Id: 'No.4',Name: 'Umashanker Gone',Email: 'umashankergone@gmail.com',
+    }
+];
+}
